d6f367afca285d67ba95c1232c2ffd1c0b9154b0
Marina Merlo <marina27merlo@gmail.com>
2019-10-16 20:43:30 -0300

    ae

--- configgithub.R
+++ configgithub.R
@@ -7,7 +7,16 @@ usethis::use_git_config(user.name = "Marina Merlo",
 usethis::browse_github_token()
 usethis::edit_r_environ()
 
+getwd()
+
 
 usethis::create_project("/Teste-MeetupR")
 
 usethis::use_git()
+usethis::use_github()
+
+## forkando e clonando do githug
+
+usethis::create_from_github(repo_spec = "JaTvoiRabotnik/SeattleDrugCrime",
+                            destdir = "C:/Users/marin/Dropbox/R-Ladies",
+                            fork = T)
